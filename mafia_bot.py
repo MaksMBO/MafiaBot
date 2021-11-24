e181a8d85ced04744a9e9c1ba97be4c47556da6d
Viacheslav Moskalenko <xomkaxomiakovuch@gmail.com>
2021-11-24 14:59:32 +0200

    try

--- mafia_bot.py
+++ mafia_bot.py
@@ -2,39 +2,58 @@ import config
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 import markup
+import json
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
+async def update_data(users, user):
+    if str(user.id) not in users:
+        users[user.id] = {}
+        users[user.id]['Games played'] = 0
+        users[user.id]['Games won'] = 0
+
+
+async def add_games_played(users, user, game_result):
+    """add number of games that he played"""
+    users[str(user.id)][game_result] += 1
+
+
+@dp.message_handler(commands=['game'])
+async def send_invite(message: types.Message):
+    await message.reply("Registration is open", reply_markup=markup.inline_keyboard_join)
+
+
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.delete()
-    await message.reply(" ")
-    await bot.send_message(message.from_user.id,
-                           "Hi! {0.first_name}, I'm mafia bot!".format(
-                               message.from_user), reply_markup=markup.mainMenu)
+async def send_welcome_message(message: types.Message):
+    await bot.send_message(message.from_user.id, "Hi! {0.first_name}, 🕴️ I'm mafia bot ".format(message.from_user),
+                           reply_markup=markup.inline_keyboard_start)
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    await update_data(users, message.from_user)
+    with open('users.json', 'w') as f:
+        json.dump(users, f, indent=4)
 
 
-@dp.message_handler()
-async def creating_buttons(message: types.Message):
-    if message.text == 'Profile🤵🏻':
-        await message.delete()
-        await bot.send_message(message.from_user.id, "👤*{0.first_name}*\n\nGames played: \nGames won: ".format(
-            message.from_user), reply_markup=markup.menu2, parse_mode="Markdown")
-    if message.text == '🔙Back':
-        await message.delete()
-        await bot.send_message(message.from_user.id, "M", reply_markup=markup.mainMenu)
+@dp.callback_query_handler(text="Profile🤵🏻")
+async def creating_buttons(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(call.from_user.id, "👤*{0.first_name}*\n\nGames played: \nGames won: ".format(
+        call.from_user), reply_markup=markup.inline_keyboard_back, parse_mode="Markdown")
 
 
+@dp.callback_query_handler(text="Help")
+async def creating_buttons(call: types.CallbackQuery):
+    await bot.send_message(call.from_user.id, "jhu")
 
-# async def update_data(users, user, server):
-#     users[str(server.id)] = {}
-#     if not str(user.id) in users[str(server.id)]:
-#         users[str(server.id)][str(user.id)] = {}
-#     elif not str(user.id) in users[str(server.id)]:
-#         users[str(server.id)][str(user.id)] = {}
+
+@dp.callback_query_handler(text="🔙")
+async def creating_buttons(call: types.CallbackQuery):
+    await call.message.delete()
+    await bot.send_message(call.from_user.id, "👤*{0.first_name}*\n\nGames played: \nGames won: ".format(
+        call.from_user), reply_markup=markup.inline_keyboard_start, parse_mode="Markdown")
 
 
 if __name__ == '__main__':
--- markup.py
+++ markup.py
@@ -1,8 +1,13 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-btnAddBot = KeyboardButton("Add bot to server", url='https://t.me/CeeebBot?startgroup=true')
-btnProfile = KeyboardButton('Profile🤵🏻')
-btnHelp = KeyboardButton('Help')
-btnBack = KeyboardButton('🔙Back')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnAddBot, btnProfile, btnHelp)
-menu2 = ReplyKeyboardMarkup(resize_keyboard=True).add(btnBack)
+"""INLINE KEYBOARD BUTTONS"""
+inline_button_start = InlineKeyboardButton("Join🤵🏻", url='https://t.me/CeeebBot')
+inline_keyboard_join = InlineKeyboardMarkup(row_width=2).add(inline_button_start)
+
+inline_button_profile = InlineKeyboardButton('Profile🤵🏻', callback_data='Profile🤵🏻')
+inline_button_help = InlineKeyboardButton('Help', callback_data='Help')
+inline_button_back = InlineKeyboardButton('🔙', callback_data='🔙')
+inline_button_addBot = InlineKeyboardButton("Add bot to server🕵🏻‍♂️", url='https://t.me/CeeebBot?startgroup=true')
+inline_keyboard_start = InlineKeyboardMarkup(row_width=2).add(inline_button_addBot, inline_button_profile,
+                                                              inline_button_help)
+inline_keyboard_back = InlineKeyboardMarkup(row_width=2).add(inline_button_back)
