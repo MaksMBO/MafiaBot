8a102a0a07a6be998160c7b3359c7e0c3266800c
MaksMBO <Maksbredyk@gmail.com>
2021-11-25 15:40:47 +0200

    init

---
+++ Game.py
@@ -0,0 +1,37 @@
+class Game:
+    def give_roles(self):
+        """
+        Create a list with player id
+
+        random roles
+
+        create a dict(id is a key) with objects (roles)
+        """
+        pass
+
+    def day(self):
+        """
+
+        Start a timer for discussion
+
+        voting in private messages
+
+        if additional voting is needed, it is carried out in the general chat
+
+        """
+
+        pass
+
+    def night(self):
+        """
+
+        role function run loop
+
+        """
+        pass
+
+    def game(self):
+        with True:
+            self.night()
+            self.day()
+
