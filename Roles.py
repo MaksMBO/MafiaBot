083aa1ada9b9c85bad7f6b98454b7a0005caa908
MaksMBO <Maksbredyk@gmail.com>
2021-11-25 15:42:42 +0200

    dell a priority

--- Roles.py
+++ Roles.py
@@ -4,7 +4,6 @@ class Civilian:
         self.is_dead = False  # boolean is player alive or dead
         # self.is_mafia = False  # boolean is player mafia or not # We will use isinstance
         self.has_night_action = False  # boolean is role has night action or not
-        self.priority = 0  # i'm not sure, int??
 
     def day(self, user_id):
         pass
@@ -14,7 +13,6 @@ class Mafia(Civilian):
     def __init__(self, user_id):
         super().__init__(user_id)
         self.has_night_action = True
-        self.priority = 1
 
     def night(self, info_for_chat):
         pass
@@ -24,7 +22,6 @@ class Medic(Civilian):
     def __init__(self, user_id):
         super().__init__(user_id)
         self.has_night_action = True
-        self.priority = 2
 
     def night(self, info_for_chat):
         pass
@@ -34,8 +31,6 @@ class Police(Civilian):
     def __init__(self, user_id):
         super().__init__(user_id)
         self.has_night_action = True
-        self.priority = 3
 
     def night(self, info_for_chat):
         pass
-
