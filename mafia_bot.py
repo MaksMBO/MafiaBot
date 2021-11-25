22e37d349fad76611edf620bac1e166e03af95cc
MaksMBO <Maksbredyk@gmail.com>
2021-11-25 18:03:10 +0200

    add new start

--- mafia_bot.py
+++ mafia_bot.py
@@ -4,9 +4,20 @@ from aiogram import Bot, Dispatcher, executor, types
 import markup
 import json
 
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+# from aiogram.utils.helper import Helper, HelperMode, ListItem
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class My_states(StatesGroup):
+    START = State()
+    # MENU = State()
 
 
 async def update_data(users, user):
@@ -26,8 +37,14 @@ async def send_invite(message: types.Message):
     await message.reply("Registration is open", reply_markup=markup.inline_keyboard_join)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start'], state=My_states.START)
+async def aboba(message: types.Message):
+    await bot.send_message(message.from_user.id, "a".format(message.from_user))
+
+
+@dp.message_handler(commands=['start'], state=None)
 async def send_welcome_message(message: types.Message):
+    await My_states.START.set()
     await bot.send_message(message.from_user.id, "Hi! {0.first_name}, 🕴️ I'm mafia bot ".format(message.from_user),
                            reply_markup=markup.inline_keyboard_start)
     with open('users.json', 'r') as f:
@@ -37,23 +54,24 @@ async def send_welcome_message(message: types.Message):
         json.dump(users, f, indent=4)
 
 
-@dp.callback_query_handler(text="Profile🤵🏻")
+@dp.callback_query_handler(text="Profile🤵🏻", state=My_states.START)
 async def creating_buttons(call: types.CallbackQuery):
     await call.message.delete()
     await bot.send_message(call.from_user.id, "👤*{0.first_name}*\n\nGames played: \nGames won: ".format(
         call.from_user), reply_markup=markup.inline_keyboard_back, parse_mode="Markdown")
 
 
-@dp.callback_query_handler(text="Help")
+@dp.callback_query_handler(text="Help", state=My_states.START)
 async def creating_buttons(call: types.CallbackQuery):
-    await bot.send_message(call.from_user.id, "jhu")
+    await bot.send_message(call.from_user.id, "It`s help")
 
 
-@dp.callback_query_handler(text="🔙")
+@dp.callback_query_handler(text="🔙", state=My_states.START)
 async def creating_buttons(call: types.CallbackQuery):
     await call.message.delete()
     await bot.send_message(call.from_user.id, "👤*{0.first_name}*\n\nGames played: \nGames won: ".format(
         call.from_user), reply_markup=markup.inline_keyboard_start, parse_mode="Markdown")
+    # await My_states.
 
 
 if __name__ == '__main__':
