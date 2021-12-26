00212f1d2665c15c93029b5bad6994f4908d42e4
MaksMBO <Maksbredyk@gmail.com>
2021-12-26 19:17:20 +0200

    final

---
+++ test.py
@@ -0,0 +1,14 @@
+# i = True
+# g = {}
+# g[1] = i
+# i = False
+# g[1] = i
+# g['1'] = True
+# print(g)
+#
+# if 3 not in g.keys():
+#     print("suka")
+
+g = {}
+g[-12345678900987634567890] = 124
+print(g)
